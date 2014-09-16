745f0a0343749070e5195fd6324363ddc8a9b5ed
Jason Humphries <humphriesj@gmail.com>
2014-09-16 09:34:07 -0400

    Initial Commit

---
+++ Plates/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Plates
+//
+//  Created by Jason Humphries on 16/09/2557 BE.
+//  Copyright (c) 2557 BE Jason Humphries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
